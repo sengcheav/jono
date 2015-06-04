d863dfefc57977b5b2bb3de482b01aafd4e0827d
Ryan Griffin <griffiryan@plum.ecs.vuw.ac.nz>
2015-06-04 18:20:57 +1200

    90876z6676

--- quote.js
+++ quote.js
@@ -161,13 +161,13 @@ app.post('/login',function(request,response){
 
 
 app.post('/logout',function(request,response){
-
-  if(!(tokenAllowed(request.param('token')))){
+  console.log('gggggggggggggggg' + request.params.token);
+  if(!(tokenAllowed(request.params.token))){
     response.statusCode = 400;
     return response.send('Invalid Access token!');
   }
 
-  removeActiveToken(request.param('token'));
+  removeActiveToken(request.params.token);
   response.statusCode = 200;
   return response.send(null)
 
