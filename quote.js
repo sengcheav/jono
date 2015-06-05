d6f0bd67aaddcaf4662f6f2965c3c64ce7913d04
Ryan Griffin <griffiryan@logan-brown.ecs.vuw.ac.nz>
2015-06-05 15:19:50 +1200

    69fr55gtuk;tuip;ggggggttt55yyyyy556666667567[556789yyrtysxfgjsxfgjhtyrg

--- quote.js
+++ quote.js
@@ -110,7 +110,7 @@ function giveMeAToken(given){
 
 function tokenAllowed(given,callback){
 
-  query = client.query('SELECT COUNT(token) FROM validTokens v WHERE v.token = $1',[given]);
+  query = client.query('SELECT COUNT(*) FROM validTokens v WHERE v.token = $1',[given]);
 
   var count;
   query.on('row',function(result){
