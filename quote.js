a2086c7a9cbcf76c2a0b19e1723f3e055fc655c4
Ryan Griffin <griffiryan@plum.ecs.vuw.ac.nz>
2015-06-07 16:25:16 +1200

    112231jy6jujjytttttuiyhhth

--- quote.js
+++ quote.js
@@ -223,7 +223,6 @@ function doAll(req,res){
     else{
       res.writeHead(200);
       res.write(JSON.stringify(results.map(function (results){ return {author: results.author, content: results.content}; })));
-      res.write(accepted = true);
       res.end();
     }
   });
