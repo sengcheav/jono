4ed1094f0c251079e63436057cd365a489598eb8
Ryan Griffin <griffiryan@gotham.ecs.vuw.ac.nz>
2015-06-19 15:45:41 +1200

    rgtyutgvbbhghbbh

--- quote.js
+++ quote.js
@@ -59,7 +59,7 @@ app.post('/login',function(req,res){
     if(count != 0){
       var query2 = client.query('INSERT INTO validTokens(token) VALUES($1)', [token],function(){
         res.writeHead(200);
-        res.write(token);
+        //res.write(token);
         res.sendFile(path.join(__dirname, '/www', 'html/title.html'));  //redirects 
         res.end();
       });
