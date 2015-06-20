e2c541f8d6f9472e799731e808b138415aeb5560
Guest User <Guest@i-03-06-lab.mddn.vuw.ac.nz>
2015-06-20 13:05:29 +1200

    hyhyh

--- register.js
+++ register.js
@@ -8,8 +8,12 @@ var pg = require('pg').native
 client = new pg.Client(connectionString);
 client.connect();
 
+var un = 'test';
+var pw = '1234';
+
 //CREATE A SCHEMA - users
-query = client.query('CREATE TABLE users(username text PRIMARY KEY, password text NOT NULL)');
+//query = client.query('CREATE TABLE users(username text PRIMARY KEY, password text NOT NULL)');
+var query2 = client.query('INSERT INTO users(username,password) VALUES($1,$2)',[un,pw]);
 
 //CALLBACK TO END DATABASE CONNECTION
 query.on('end', function(result) { client.end(); });
